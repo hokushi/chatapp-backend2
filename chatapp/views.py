edf29f039908b67e500e2c49e70492d97cc9007e
hokushi <hokushi97@gmail.com>
2023-02-09 02:20:48 +0900

    月と日を追加

--- chatapp/views.py
+++ chatapp/views.py
@@ -10,9 +10,10 @@ def get_user(request, user_id):
     user = ChatappUser.objects.get(id=user_id)
     data = {
         'name': user.name,
-        'id':user_id,
+        'id': user_id,
     }
     return JsonResponse(data)
+
 
 @csrf_exempt
 def message(request):
@@ -24,22 +25,26 @@ def message(request):
                 'message': message.message,
                 'sendername': message.sendername.name,
                 'sendername_id': message.sendername.id,
-                'created_at': [timezone.localtime(message.created_at).hour,
-                            timezone.localtime(message.created_at).minute,]
+                'created_at': [timezone.localtime(message.created_at).month,
+                               timezone.localtime(message.created_at).day,
+                               timezone.localtime(message.created_at).hour,
+                               timezone.localtime(message.created_at).minute,]
             })
         return JsonResponse(data, safe=False)
-    
+
     if request.method == 'POST':
         datas = json.loads(request.body)
         chatappUser_id = datas['chatappUser_id']
         message = datas['message']
-        ChatappMessage.objects.create(sendername=ChatappUser.objects.get(id=chatappUser_id), message=message)
+        ChatappMessage.objects.create(
+            sendername=ChatappUser.objects.get(id=chatappUser_id), message=message)
         return HttpResponse('message登録完了！')
 
     if request.method == 'DELETE':
         chat_app_message = ChatappMessage.objects.get(id=user_id)
         chat_app_message.delete()
         return HttpResponse("削除に成功しました")
+
 
 def get_messages_of_user(request, user_id):
     messages_list = ChatappUser.objects.get(id=user_id).ChatappMessages.all()
